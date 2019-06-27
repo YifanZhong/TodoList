94fc688f8c06ebc21bbad0b093d98e1949df06a1
Ivan Zhong <ivanzhong0310@gmail.com>
2019-06-27 14:21:01 +1200

    use antd to create an input text field and a submit button

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Input, Button} from 'antd';
 
 class TodoList extends Component {
     render(){
-        return <div>hello world</div>
+        return (
+            <div style = {{marginTop: '10px', marginRight: '10px'}}>
+                <Input
+                    placeholder = 'todo_infomation'
+                    style = {{width: '300px', marginRight: '10px'}}
+                />
+                <Button type="primary">submit</Button>
+            </div>
+        )
     }
 }
 
